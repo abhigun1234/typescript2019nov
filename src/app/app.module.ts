ca9d235b89bba4f198c1f7f04f7c8599c9bf0d5d
Gungun <gungun@Gunguns-MacBook-Pro.local>
2019-12-01 09:15:32 +0530

    adding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BootstrapdemoComponent } from './bootstrapdemo/bootstrapdemo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BootstrapdemoComponent
   ],
   imports: [
     BrowserModule,
